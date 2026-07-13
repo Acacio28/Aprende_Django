1b16b8978cc826a120c34e3e402b13569b9e0fcd
Akashio28 <acacioelezito933@gmail.com>
2026-07-13 22:04:27 +0900

    feat: add custom 404 and 500 error pages

--- Kursu/urls.py
+++ Kursu/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404, handler500
 from Kursu import views
 
 urlpatterns = [
@@ -9,3 +10,6 @@ urlpatterns = [
     path('docente/', include('docente.urls')),
     path('akademiku/', include('akademiku.urls')),
 ]
+
+handler404 = 'Kursu.views.handler404'
+handler500 = 'Kursu.views.handler500'
--- Kursu/views.py
+++ Kursu/views.py
@@ -30,3 +30,11 @@ def dashboard(request):
     except LookupError:
         context['turma_counts'] = []
     return render(request, 'dashboard.html', context)
+
+
+def handler404(request, exception):
+    return render(request, 'errors/404.html', status=404)
+
+
+def handler500(request):
+    return render(request, 'errors/500.html', status=500)
