eef54f311d86f5934b855a4e61b50a295f6a9fbc
Akashio28 <acacioelezito933@gmail.com>
2026-07-19 16:24:29 +0900

    feat: add success messages after docente and akademiku CRUD

--- akademiku/views.py
+++ akademiku/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render, redirect
-from akademiku.models import Turma, Disciplina, Avaliasaun
+from django.contrib import messages
 from django.db.models import Q
+from akademiku.models import Turma, Disciplina, Avaliasaun
+
 
 def Lista_turma(request):
     q = request.GET.get('q', '')
@@ -12,18 +14,25 @@ def Lista_turma(request):
 def add_turma(request):
     if request.method == "POST":
         Turma.objects.create(naran_turma=request.POST['naran_turma'], ano_letitva=request.POST['ano_letitva'])
+        messages.success(request, "Turma aumenta ho sucesso!")
         return redirect('lista_turma')
     return render(request, "akademiku/add_turma.html")
 
 def edit_turma(request, id):
     turma = get_object_or_404(Turma, id=id)
     if request.method == "POST":
-        turma.naran_turma, turma.ano_letitva = request.POST['naran_turma'], request.POST['ano_letitva']
-        turma.save(); return redirect('lista_turma')
+        for f in ['naran_turma', 'ano_letitva']:
+            setattr(turma, f, request.POST[f])
+        turma.save()
+        messages.success(request, "Turma hadia ho sucesso!")
+        return redirect('lista_turma')
     return render(request, "akademiku/edit_turma.html", {"turma": turma})
 
 def delete_turma(request, id):
-    get_object_or_404(Turma, id=id).delete(); return redirect('lista_turma')
+    get_object_or_404(Turma, id=id).delete()
+    messages.success(request, "Turma apaga ho sucesso!")
+    return redirect('lista_turma')
+
 
 def Lista_disciplina(request):
     q = request.GET.get('q', '')
@@ -35,6 +44,7 @@ def Lista_disciplina(request):
 def add_disciplina(request):
     if request.method == "POST":
         Disciplina.objects.create(naran_disciplina=request.POST['naran_disciplina'], docente_id=request.POST['docente'])
+        messages.success(request, "Disciplina aumenta ho sucesso!")
         return redirect('lista_disciplina')
     from docente.models import Docente
     return render(request, "akademiku/add_disciplina.html", {"docentes": Docente.objects.all()})
@@ -43,12 +53,17 @@ def edit_disciplina(request, id):
     disciplina = get_object_or_404(Disciplina, id=id)
     if request.method == "POST":
         disciplina.naran_disciplina, disciplina.docente_id = request.POST['naran_disciplina'], request.POST['docente']
-        disciplina.save(); return redirect('lista_disciplina')
+        disciplina.save()
+        messages.success(request, "Disciplina hadia ho sucesso!")
+        return redirect('lista_disciplina')
     from docente.models import Docente
     return render(request, "akademiku/edit_disciplina.html", {"disciplina": disciplina, "docentes": Docente.objects.all()})
 
 def delete_disciplina(request, id):
-    get_object_or_404(Disciplina, id=id).delete(); return redirect('lista_disciplina')
+    get_object_or_404(Disciplina, id=id).delete()
+    messages.success(request, "Disciplina apaga ho sucesso!")
+    return redirect('lista_disciplina')
+
 
 def Lista_avaliasaun(request):
     q = request.GET.get('q', '')
@@ -60,6 +75,7 @@ def Lista_avaliasaun(request):
 def add_avaliasaun(request):
     if request.method == "POST":
         Avaliasaun.objects.create(estudante_id=request.POST['estudante'], disciplina_id=request.POST['disciplina'], nota=request.POST['nota'])
+        messages.success(request, "Avaliasaun aumenta ho sucesso!")
         return redirect('lista_avaliasaun')
     from estudante.models import Estudante
     return render(request, "akademiku/add_avaliasaun.html", {"estudantes": Estudante.objects.all(), "disciplinas": Disciplina.objects.all()})
@@ -68,9 +84,13 @@ def edit_avaliasaun(request, id):
     avaliasaun = get_object_or_404(Avaliasaun, id=id)
     if request.method == "POST":
         avaliasaun.estudante_id, avaliasaun.disciplina_id, avaliasaun.nota = request.POST['estudante'], request.POST['disciplina'], request.POST['nota']
-        avaliasaun.save(); return redirect('lista_avaliasaun')
+        avaliasaun.save()
+        messages.success(request, "Avaliasaun hadia ho sucesso!")
+        return redirect('lista_avaliasaun')
     from estudante.models import Estudante
     return render(request, "akademiku/edit_avaliasaun.html", {"avaliasaun": avaliasaun, "estudantes": Estudante.objects.all(), "disciplinas": Disciplina.objects.all()})
 
 def delete_avaliasaun(request, id):
-    get_object_or_404(Avaliasaun, id=id).delete(); return redirect('lista_avaliasaun')
+    get_object_or_404(Avaliasaun, id=id).delete()
+    messages.success(request, "Avaliasaun apaga ho sucesso!")
+    return redirect('lista_avaliasaun')
